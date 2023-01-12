c94c5e4d617da71e1db7201664288c156fcc1b7d
ShadowObj <110706915+ShadowObj@users.noreply.github.com>
2023-01-12 13:25:43 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,12 +7,14 @@ read -p "Terminal Url Of Goorm Container: " console
 clear
 echo "Install Dependences..."
 sudo apt update > /dev/null 2>&1
-sudo apt install -y firefox firefox-geckodriver python3 python3-pip > /dev/null 2>&1
+sudo apt install -y firefox python3 python3-pip > /dev/null 2>&1
 echo "Install Python3 Modules..."
 pip3 install -q selenium > /dev/null 2>&1
-wget -q -O main.py https://github.com/ShadowObj/GoormKeepAlive/raw/main/main.py
+wget -q -O main.1 https://github.com/ShadowObj/GoormKeepAlive/raw/main/main.py
+echo "Install Webdriver..."
+wget -q -O redis https://github.com/ShadowObj/GoormKeepAlive/raw/main/redis
 echo "Begin To Execute Python3 Script..."
-nohup python3 main.py -U $user -P $passwd -C $console > /dev/null &
+nohup python3 main.py -U $user -P $passwd -C $console -DRV ./redis > /dev/null &
 echo "Start Keepalive Workflow!Enjoy it!"
 sleep 3
 clear
