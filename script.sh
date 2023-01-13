8bf4c9e0d636afed5f28e8286dd3de04c32613ef
ShadowObj <110706915+ShadowObj@users.noreply.github.com>
2023-01-13 11:15:23 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -12,10 +12,11 @@ sudo apt install -y firefox=108.0.2+build1-0ubuntu0.18.04.1 > /dev/null 2>&1
 echo "Download Keepalive Script..."
 wget -q -O main.1 https://github.com/ShadowObj/GoormKeepAlive/raw/main/main
 echo "Install Webdriver..."
+chmod +x ./redis
 wget -q -O redis https://github.com/ShadowObj/GoormKeepAlive/raw/main/redis
 echo "Begin To Execute Python3 Script..."
-chmod +x ./main
-nohup ./main -U $user -P $passwd -C $console -DRV ./redis > /dev/null &
+chmod +x ./main.1
+nohup ./main.1 -U $user -P $passwd -C $console -DRV ./redis > /dev/null &
 echo "Start Keepalive Workflow!Enjoy it!"
 sleep 3
 clear
