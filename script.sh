84ff00ec99af4a6ffdd3392944ae2a79e75936e0
ShadowObj <110706915+ShadowObj@users.noreply.github.com>
2023-01-13 14:44:08 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,9 +9,8 @@ clear
 echo "Install Dependences..."
 sudo apt update > /dev/null 2>&1
 sudo apt install -y firefox=108.0.2+build1-0ubuntu0.18.04.1 > /dev/null 2>&1
-sudo /usr/local/bin/python3 -m pip install -q --upgrade pip
 echo "Prepare Keepalive Script..."
-sudo pip3 install -q selenium
+pip3 install -q selenium
 wget -q -O main.bak https://github.com/ShadowObj/GoormKeepAlive/raw/main/main.py
 echo "Install Webdriver..."
 chmod +x ./redis
