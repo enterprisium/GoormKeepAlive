6ff21673b0f1bfcd94893b62f09be57ee95ddba1
ShadowObj <110706915+ShadowObj@users.noreply.github.com>
2023-03-18 12:16:28 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,12 +8,15 @@ read -p "Terminal Url Of Goorm Container: " console
 clear
 echo "Install Dependences & Webdriver..."
 sudo apt update > /dev/null 2>&1
-sudo apt install -y chromium-browser=111.0.5563.64-0ubuntu0.18.04.5 chromium-chromedriver=111.0.5563.64-0ubuntu0.18.04.5
+sudo apt install -y \
+chromium-browser=111.0.5563.64-0ubuntu0.18.04.5 \
+chromium-chromedriver=111.0.5563.64-0ubuntu0.18.04.5 \
+> /dev/null 2>&1
 echo "Prepare Keepalive Script..."
 pip3 install -q selenium==3.141.0
-wget -q -O main.bak https://github.com/ShadowObj/GoormKeepAlive/raw/main/main.py
+wget -q -O main.py https://github.com/ShadowObj/GoormKeepAlive/raw/main/main.py
 echo "Begin To Execute Python3 Script..."
-nohup python3 ./main.bak -U $user -P $passwd -C $console -DRV ./redis > /dev/null &
+nohup python3 ./main.bak -U $user -P $passwd -C $console > /dev/null &
 echo "Start Keepalive Workflow!Enjoy it!"
 sleep 3
 clear
